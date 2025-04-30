07beb31bd52161a8bbcf5506a8b2ff9532949be5
rderickson88 <erickryanson@gmail.com>
2025-04-30 10:33:54 -0400

    Super second method!

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/elms-nursing-patient-eval-app/',
   plugins: [react()],
+  base: '/elms-nursing-patient-eval-app',
 })
